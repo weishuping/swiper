100bcfcd4d47c9cd8139c8cca06cd5347ad965f6
weishuping <weishp3@yonyou.com>
2017-07-06 14:50:18 +0800

    增加首页，等待麦田给图，把第一个的图换一换

--- mt/js/index.js
+++ mt/js/index.js
@@ -4,7 +4,7 @@ var mySwiperV1 = new Swiper ('.swiper-container-v1', {
     pagination: '.swiper-pagination-v1',
     loop: false,
     observer: true, //修改swiper自己或子元素时，自动初始化swiper
-        observeParents: true, //修改swiper的父元素时，自动初始化swiper
+    observeParents: true, //修改swiper的父元素时，自动初始化swiper
     onInit: function(swiper){
         swiperAnimateCache(swiper);
         swiperAnimate(swiper);
@@ -19,7 +19,7 @@ var mySwiperV2 = new Swiper ('.swiper-container-v2', {
     pagination: '.swiper-pagination-v2',
     loop: false,
     observer: true, //修改swiper自己或子元素时，自动初始化swiper
-        observeParents: true, //修改swiper的父元素时，自动初始化swiper
+    observeParents: true, //修改swiper的父元素时，自动初始化swiper
     onInit: function(swiper){
         swiperAnimateCache(swiper);
         swiperAnimate(swiper);
@@ -28,52 +28,59 @@ var mySwiperV2 = new Swiper ('.swiper-container-v2', {
         swiperAnimate(swiper);
     }
 });
-//横向
-var mySwiperH = new Swiper ('.swiper-container-h', {
+// /*横向 */纵向
+var mySwiperV = new Swiper ('.swiper-container-v', {
+    direction : 'vertical',
     loop: false,
     observer: true, //修改swiper自己或子元素时，自动初始化swiper
-        observeParents: true, //修改swiper的父元素时，自动初始化swiper
+    observeParents: true, //修改swiper的父元素时，自动初始化swiper
     onInit: function(swiper){
         swiperAnimateCache(swiper);
         swiperAnimate(swiper);
     },
     onSlideChangeEnd: function(swiper){
-        swiperAnimate(swiper);
         //滑动之后横向滑动锁住
-        mySwiperH.lockSwipeToNext();
-        mySwiperH.lockSwipeToPrev();
+        if(swiper.activeIndex === 1) {
+            mySwiperV.lockSwipeToNext();
+            $('.array-h').hide();
+            //mySwiperV.lockSwipeToPrev();
+        } else {
+            mySwiperV.unlockSwipeToNext();
+            $('.array-h').show();
+        }
+        swiperAnimate(swiper);
     }
 });
 //初始页面横向滑动锁住
-mySwiperH.lockSwipeToNext();
-mySwiperH.lockSwipeToPrev();
+// mySwiperH.lockSwipeToNext();
+// mySwiperH.lockSwipeToPrev();
 //初始化详情页面不可见
-$('.details').hide();
+$('.details').hide(); 
 //预制门店顾问入职数据
 var s1 ='当事人招募部面熟通过；门店面试通过'
 //点击1 总部员工入职
 $('.map2_s1').on('click', function(e){
-    mySwiperH.unlockSwipeToNext();
-    mySwiperH.slideTo(1,200,true);
+    mySwiperV.unlockSwipeToNext();
+    mySwiperV.slideTo(2,200,true);
     
 });
 //点击2 门店顾问入职
 $('.map2_s2').on('click', function(e){
-    mySwiperH.unlockSwipeToNext();
-    mySwiperH.slideTo(2,200,true);
+    mySwiperV.unlockSwipeToNext();
+    mySwiperV.slideTo(3,200,true);
 });
 //1 返回首页
 $('.backHome').on('click', function(e){
     //$('#showhide').hide();
     //mySwiperV1.activeIndex = 0;
-    mySwiperH.unlockSwipeToPrev();
-    mySwiperH.slideTo(0,200,true);
+    mySwiperV.unlockSwipeToPrev();
+    mySwiperV.slideTo(1,200,true);
     mySwiperV1.activeIndex = 0;
 });
 //2 返回首页
 $('.backHome2').on('click', function(e){
-    mySwiperH.unlockSwipeToPrev();
-    mySwiperH.slideTo(0,200,true);
+    mySwiperV.unlockSwipeToPrev();
+    mySwiperV.slideTo(1,200,true);
     mySwiperV2.activeIndex = 0;
 });
 //跳转详情页
